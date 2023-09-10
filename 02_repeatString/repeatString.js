36eba70ad672b524a23eeaa5688af3a15dbf18fd
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-10 10:40:07 -0600

    Implemented solutions for first 4 exercises. All tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
-};
+const repeatString = (str, num) => {
+    let ans = ""
+    if (num > 0) {
+      for (let i = 0; i < num; i++)
+        {
+          ans += str;
+        }
+    } else if (num == 0) {
+        return ans;
+    } else {
+        return "ERROR";
+    }
+    return ans;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elim) {
+    let answer = [];
+    for (const element of arr) {
+        if (!elim.includes(element)) {
+            answer.push(element);
+        }
+    }
+    return answer;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 4, 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
